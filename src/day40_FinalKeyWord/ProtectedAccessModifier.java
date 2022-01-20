9679e33b06315852f217b8050199e9a89394e986
arisi <nattayasdet@gmail.com>
2022-01-20 11:03:24 -0500

    step 4 is done

---
+++ src/day40_FinalKeyWord/ProtectedAccessModifier.java
@@ -0,0 +1,18 @@
+package day40_FinalKeyWord;
+
+public class ProtectedAccessModifier {
+
+    public static void main(String[] args) {
+
+
+        static String name1 = "Cydeo";
+        protected String name2 = "WoodenSpoon";
+    }
+    static void method1(){
+
+    }
+
+    protected static void method2(){
+
+    }
+}
---
+++ src/utilities/ProtectedAccessModifier_Test4.java
@@ -0,0 +1,10 @@
+package utilities;
+
+public class ProtectedAccessModifier_Test4 extends day40_FinalKeyWord.ProtectedAccessModifier {
+
+    public static void main(String[] args) {
+
+        System.out.println(AccessModifier_Test4.);
+        System.out.println(name2);
+    }
+}
